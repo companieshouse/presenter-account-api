2e739e2aa7b9f731b75ddc8ee1717cbb16fb5aec
ch-skathirvel <136324774+ch-skathirvel@users.noreply.github.com>
2024-01-15 16:20:51 +0000

    JIRA : AOAF-277 (#19)
    JIRA : AOAF-277
    Description : Added internal user interceptor for the service and excluded post call url from internal user interceptor.

--- src/main/java/uk/gov/companieshouse/presenter/account/configuration/WebMvcConfig.java
+++ src/main/java/uk/gov/companieshouse/presenter/account/configuration/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
 import uk.gov.companieshouse.api.interceptor.TokenPermissionsInterceptor;
 import uk.gov.companieshouse.presenter.account.interceptor.LoggingInterceptor;
 import uk.gov.companieshouse.presenter.account.interceptor.UserAuthenticationInterceptor;
@@ -13,12 +14,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final LoggingInterceptor loggingInterceptor;
     private final UserAuthenticationInterceptor userAuthenticationInterceptor;
+    private final InternalUserInterceptor internalUserInterceptor;
 
     @Autowired
     public WebMvcConfig(final LoggingInterceptor loggingInterceptor,
-                        final UserAuthenticationInterceptor userAuthenticationInterceptor) {
+                        final UserAuthenticationInterceptor userAuthenticationInterceptor,
+                        final InternalUserInterceptor internalUserInterceptor) {
         this.loggingInterceptor = loggingInterceptor;
         this.userAuthenticationInterceptor = userAuthenticationInterceptor;
+        this.internalUserInterceptor = internalUserInterceptor;
     }
 
     @Override
@@ -26,8 +30,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(loggingInterceptor);
         registry.addInterceptor(getTokenPermissionsInterceptor());
         registry.addInterceptor(userAuthenticationInterceptor)
-                .excludePathPatterns(
-                        "/presenter-account/healthcheck");
+                .excludePathPatterns("/presenter-account/healthcheck");
+        registry.addInterceptor(internalUserInterceptor)
+                .excludePathPatterns("/presenter-account/healthcheck")
+                .excludePathPatterns("/presenter-account/");
     }
 
     private TokenPermissionsInterceptor getTokenPermissionsInterceptor() {
