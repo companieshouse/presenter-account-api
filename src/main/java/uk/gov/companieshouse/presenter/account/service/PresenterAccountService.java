d9c3300da64910fe8ae50a19b1fe808e9ebd293b
wdhda-ch <awoodhead@companieshouse.gov.uk>
2024-01-24 17:07:37 +0000

    adding finals

--- src/main/java/uk/gov/companieshouse/presenter/account/service/PresenterAccountService.java
+++ src/main/java/uk/gov/companieshouse/presenter/account/service/PresenterAccountService.java
@@ -3,9 +3,9 @@ package uk.gov.companieshouse.presenter.account.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.presenter.account.mapper.AdditionalIdMapper;
 import uk.gov.companieshouse.presenter.account.mapper.request.PresenterAccountDetailsMapper;
 import uk.gov.companieshouse.presenter.account.model.PresenterAccountDetails;
-import uk.gov.companieshouse.presenter.account.model.mapper.presenter.account.mapper.base.AdditionalIdMapper;
 import uk.gov.companieshouse.presenter.account.model.request.PresenterAccountDetailsRequest;
 import uk.gov.companieshouse.presenter.account.repository.PresenterAccountRepository;
 import uk.gov.companieshouse.presenter.account.utils.IdGenerator;
@@ -17,35 +17,35 @@ public class PresenterAccountService {
 
     private static final String DB_ERROR_MSG = "Error occurred while fetching presenter account from the DB.";
 
-    private Logger logger;
+    private final Logger logger;
 
-    private AdditionalIdMapper<PresenterAccountDetails, PresenterAccountDetailsRequest> detailsMapper;
+    private final AdditionalIdMapper<PresenterAccountDetails, PresenterAccountDetailsRequest> detailsMapper;
 
-    private PresenterAccountRepository presenterAccountRepository;
+    private final PresenterAccountRepository presenterAccountRepository;
 
-    private IdGenerator idGenerator;
+    private final IdGenerator idGenerator;
 
     @Autowired
-    public PresenterAccountService(Logger logger, PresenterAccountDetailsMapper detailsMapper,
-            PresenterAccountRepository presenterAccountRepository, IdGenerator idGenerator) {
+    public PresenterAccountService(final Logger logger, final PresenterAccountDetailsMapper detailsMapper,
+            final PresenterAccountRepository presenterAccountRepository, final IdGenerator idGenerator) {
         this.logger = logger;
         this.detailsMapper = detailsMapper;
         this.presenterAccountRepository = presenterAccountRepository;
         this.idGenerator = idGenerator;
     }
 
-    public String createPresenterAccount(PresenterAccountDetailsRequest presenterAccountDetailsRequest) {
-        PresenterAccountDetails presenterDetails = detailsMapper.map(idGenerator.createUUID(),
+    public String createPresenterAccount(final PresenterAccountDetailsRequest presenterAccountDetailsRequest) {
+        final PresenterAccountDetails presenterDetails = detailsMapper.map(idGenerator.createUUID(),
                 presenterAccountDetailsRequest);
         presenterAccountRepository.save(presenterDetails);
         logger.info("Presenter account details has been added to the database.");
         return presenterDetails.presenterDetailsId();
     }
 
-    public Optional<PresenterAccountDetails> getPresenterAccount(String presenterDetailsId) {
+    public Optional<PresenterAccountDetails> getPresenterAccount(final String presenterDetailsId) {
         try {
             return presenterAccountRepository.findById(presenterDetailsId);
-        } catch (Exception e) {
+        } catch (final Exception e) {
             logger.error(DB_ERROR_MSG, e);
             throw e;
         }
