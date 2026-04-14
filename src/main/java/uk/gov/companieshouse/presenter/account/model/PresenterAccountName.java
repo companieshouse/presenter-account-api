ff68e952397c58af52c8e3c880caf6e080103319
mbuleandra-ch <mbuleandra@companieshouse.gov.uk>
2026-04-14 17:07:43 +0100

    Making presenter account forename and surname mandatory when calling the API directly

--- src/main/java/uk/gov/companieshouse/presenter/account/model/PresenterAccountName.java
+++ src/main/java/uk/gov/companieshouse/presenter/account/model/PresenterAccountName.java
@@ -2,30 +2,26 @@ package uk.gov.companieshouse.presenter.account.model;
 
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import org.springframework.lang.Nullable;
 import uk.gov.companieshouse.presenter.account.exceptionhandler.ValidationException;
 import uk.gov.companieshouse.presenter.account.validation.utils.StringValidator;
 
 public record PresenterAccountName(
-        @Field("forename") @Nullable String forename,
-        @Field("surname") @Nullable String surname) {
+        @Field("forename") String forename,
+        @Field("surname") String surname) {
 
     private static final int MAX_FORENAME_LENGTH = 32;
     private static final int MAX_SURNAME_LENGTH = 40;
 
     public PresenterAccountName(final String forename, final String surname) {
-        this.forename = validateName(forename, MAX_FORENAME_LENGTH);
-        this.surname = validateName(surname, MAX_SURNAME_LENGTH);
+        this.forename = validateName(forename, MAX_FORENAME_LENGTH, "forename");
+        this.surname = validateName(surname, MAX_SURNAME_LENGTH, "surname");
     }
 
-    private String validateName(final String name, final int maxLength) {
-        // CHS user profiles often don't have associated forename and surnames
-        // Therefore null is a valid value for the name field.
-        if (name == null || StringValidator.validateString(name, maxLength)) {
+    private String validateName(final String name, final int maxLength, final String fieldName) {
+        if (StringValidator.validateString(name, maxLength)) {
             return name;
         }
-
-        throw new ValidationException("Invalid Name");
+        throw new ValidationException("Invalid " + fieldName);
     }
 
     @Override
