2ef5b849a28155f9c0be5b572954c7abfe268b43
bcullerton <bcullerton@companieshouse.gov.uk>
2026-09-10 15:17:51 +0100

    Rework codebase to allow for removal of pstore stack config

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -17,18 +17,11 @@ locals {
   application_subnet_pattern = local.stack_secrets["application_subnet_pattern"]
   use_set_environment_files  = var.use_set_environment_files
   app_environment_filename   = "presenter-account-api.env"
-  vpc_name                   = data.aws_ssm_parameter.secret[format("/%s/%s", local.name_prefix, "vpc-name")].value
+  vpc_name                   = local.stack_secrets["vpc_name"]
 
   # Enable Eric
   use_eric_reverse_proxy    = true
   eric_port                 = "3001" # container port plus 1
-
-  # create a map of secret name => secret arn to pass into ecs service module
-  # using the trimprefix function to remove the prefixed path from the secret name
-  secrets_arn_map = {
-    for sec in data.aws_ssm_parameter.secret :
-    trimprefix(sec.name, "/${local.name_prefix}/") => sec.arn
-  }
 
   service_secrets_arn_map = {
     for sec in module.secrets.secrets :
